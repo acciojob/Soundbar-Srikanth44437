8cf1b68735db33e9d288fd1d23cbfe92be71451f
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-01-28 19:34:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function playSound(soundFile) {
+    const audio = new Audio(`sounds/${soundFile}`);
+    audio.play();
+}
+
+function stopAllSounds() {
+    const allAudioElements = document.querySelectorAll('audio');
+    allAudioElements.forEach(audio => audio.pause());
+}
